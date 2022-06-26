9b27bdf50eb5ab653ba5b62450ca5de3d423fe08
Gourav Sachdeva <gouravsachdeva95@gmail.com>
2022-06-26 11:47:07 +0530

    code formatting

--- Array & Hashing/242_valid_anagram.js
+++ Array & Hashing/242_valid_anagram.js
@@ -1,8 +1,8 @@
 //Leetcode Problem 242 -> https://leetcode.com/problems/valid-anagram/
 
-var isAnagram = function(s, t) {
-    if(s && t && (s.length === t.length)){
-        if(s.split('').sort().join('') === t.split('').sort().join('')){
+var isAnagram = function (s, t) {
+    if (s && t && (s.length === t.length)) {
+        if (s.split('').sort().join('') === t.split('').sort().join('')) {
             return true;
         } else {
             false
--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 This repository consists of all type (Easy, Medium & Hard) of leetcode program solutions using Javascript w.r.t interview prepration for Product based companies.
 
 ## Array & Hashing
+- Problem 1 - Two Sum (Return Indexes)
 - Problem 217 - Contains Duplicate
 - Problem 219 - Contains Duplicate II
 - Problem 242 - Valid Anagram
-- Problem 1 - Two Sum (Return Indexes)
 
 ## Two Pointers
 - Problem 167 - Two Sum II - Input Array Is Sorted
